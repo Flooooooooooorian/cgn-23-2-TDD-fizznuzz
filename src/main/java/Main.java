2a9c7ebd860a14043302d2776e5dbf5e6a612094
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-06-07 16:47:47 +0200

    Add return number as String

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ public class Main {
     //Die Methode fizzBuzz, nimmt eine Zahl entgegen und gibt irgendeinen String zurück (nicht null).
 
     public String fizzBuzz(int number) {
-        return "";
+        return Integer.toString(number);
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class MainTest {
@@ -7,10 +8,12 @@ public class MainTest {
     @Test
     void fizzbuzzTest() {
         //Die Methode fizzBuzz, nimmt eine Zahl entgegen und gibt irgendeinen String zurück (nicht null).
+
+        //Für gewöhnliche Zahlen (Beispielinput int 4) soll sie die Zahl als Text (Beispielergebnis String "4") zurückgeben.
         Main main = new Main();
 
-        String actual = main.fizzBuzz(10);
+        String actual = main.fizzBuzz(4);
 
-        assertNotNull(actual);
+        assertEquals("4", actual);
     }
 }
